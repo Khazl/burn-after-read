8dc5e47a3029eefebf872739d55942c866d061f4
Guido Gasper <guido.gasper@googlemail.com>
2024-01-28 18:14:58 +0100

    - Move footer to dedicated component - Add link to repository

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<p class="mt-16 text-sm text-gray-500 border-t pt-4">
+    We focus on the highest, battle proof security standards.<br>
+    Messages are transmitted and stored in encrypted form. After the set lifetime, they are irrevocably deleted.<br>
+    <br>
+    No trackers, no ads, no scripts from external service providers, no financial interest.<br>
+    This application is minimalistic and pure.<br>
+    <br>
+    The code of this application is <a href="https://github.com/Khazl/burn-after-read" target="_blank" class="text-blue-500 underline">open source</a>. Verify it yourself or host it directly on your own server.
+</p>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,15 +41,7 @@
                 </div>
             </form>
 
-            <p class="mt-16 text-sm text-gray-500 border-t pt-4">
-                We focus on the highest, battle proof security standards.<br>
-                Messages are transmitted and stored in encrypted form. After the set lifetime, they are irrevocably deleted.<br>
-                <br>
-                No trackers, no ads, no scripts from external service providers, no financial interest.<br>
-                This application is minimalistic and pure.<br>
-                <br>
-                The code of this application is <a href="#" class="text-blue-500 underline">open source</a>. Verify it yourself or host it directly on your own server.
-            </p>
+            @component('components.footer')@endcomponent
         </div>
     </body>
 </html>
